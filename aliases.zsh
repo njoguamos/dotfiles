3ee31d21323183dfa683934255f2d3e850136bd4
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-12-18 23:40:06 +0300

    Add build alias

--- aliases.zsh
+++ aliases.zsh
@@ -24,6 +24,8 @@ alias cfresh="rm -rf vendor/ composer.lock && composer install"
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias dev="npm run dev"
 alias fmt="npm run fmt"
+alias build="npm run build"
+alias preview="npm run preview"
 
 # QwikJs
 alias qn="npm run qwik new"
